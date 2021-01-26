06946ecc4bd8f48c7af5b6eed3bafcebc9e2c71d
Alex Dospinoiu <alex.dospinoiu@gmail.com>
2021-01-26 05:10:52 -0700

    Created routes folder and created the apiRoutes.js and htmlRoutes.js file. Also updated the server.js file with the correct routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 // Initialize the application to create a port
 const app = express();
@@ -8,7 +10,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+app.use(morgan('tiny'));
 
+// Routes
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 // Start up the server
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
